ac22a2bb27679364bd26d31cf50b12b860a9bc56
thmosqueiro <thiago.mosqueiro@gmail.com>
2017-01-09 12:28:35 -0800

    [server] better logging during data integration

--- server/handler_DataIntegrationSocket.py
+++ server/handler_DataIntegrationSocket.py
@@ -59,8 +59,8 @@ class dataIntegrationHandler(tornado.websocket.WebSocketHandler):
     ## THE CLIENT connected
     def open(self):
         print self.state
-        logging.info("New client connected into DataIntegration from " +str(self.request.remote_ip) )
-
+        logging.info("DataIntgr. New client connected into DataIntegration from " +str(self.request.remote_ip) )
+        
         if self.state == 0:
             self.write_message("Free")
             dataIntegrationHandler.state = 1
@@ -75,36 +75,55 @@ class dataIntegrationHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
 
         if dataIntegrationHandler.currentClient == self:
-
+            
             if message == "sending":
+                logging.info("DataIntgr. Client exporting and sending data (from " +str(self.request.remote_ip)+ ")" )
                 self.write_message("Waiting")
-
-
+                
+                
             elif 'My name:' in message:
                 self.enose_id =  int(message.split(':')[1])
-
-
+                logging.info("DataIntgr. Client identified as hal" +str(self.enose_id)+ "k (from " +str(self.request.remote_ip)+ ")" )
+                
+                
             elif message == "sent":
 		print self.enose_id
-		for file in os.listdir(self.tmpFolder):
+                logging.info("DataIntgr. Client sent data (from " +str(self.request.remote_ip)+ ")" )
+                
+		for file in os.listdir(self.tmpFolder):                    
     		    if file.startswith("NewData_"+str(self.enose_id)):
+
+                        ## Removing extension
 			filename = file[:-4]
+                        logging.info("DataIntgr. Processing file " +filename+ " (from " +str(self.request.remote_ip)+ ")" )
 			self.write_message("Processing")
+                        
+                        
+                        ## Converting into CSV file
 			ym = filename.split('_')[2]
 			dia = filename.split('_')[3]
 		        os.system("python "+self.tmpFolder+"npy2csv_convert.py "+self.tmpFolder+filename
                                   +".npy "+dia+" "+ym+" "+str(self.enose_id))
+                        
+                        ## Importing into the DATABASE
         	        self.db.copy("measurement",self.tmpFolder+filename+".csv")
+                        
+                        logging.info("DataIntgr. New data processed and stored (from " +str(self.request.remote_ip)+ ")" )
+                        
+                        ## Cleaning files
 		        os.system("mv "+self.tmpFolder+filename+ ".npy "+self.tmpFolder+filename.split('New')[1]+".npy")
 			os.system("zip "+self.tmpFolder+filename.split('New')[1]+".zip "+self.tmpFolder+filename.split('New')[1]+".npy")
                         os.system("rm -f "+self.tmpFolder+filename+ ".csv")
-
-
+                                                
+                        
+                ## Finishing the process
+                logging.info("DataIntgr. Done with newly data from " +str(self.request.remote_ip) )        
                 dataIntegrationHandler.state = 0
 
 	else:
             self.write_message("Wait for your turn. Current status: "
                                + str(dataIntegrationHandler.state) )
+            print "Client disconnected."
 
 
         return
